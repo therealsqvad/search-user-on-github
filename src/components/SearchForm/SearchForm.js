c751597b01c4edfc040d6030c1f351b4de2ad2fd
therealsqvad <nikita_klepov@mail.ru>
2019-07-04 15:16:41 +0700

    delete Typeahead and refactor code

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,62 +1,26 @@
 import React, { Component } from 'react';
-import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import FormControl from 'react-bootstrap/FormControl';
+import Spinner from 'react-bootstrap/Spinner';
 import PropTypes from 'prop-types';
 import './SearchForm.css';
-// import fetchProject from '../../services/ferchProject';
-// import GithubMenuItem from '../GithubMenuItem';
-// import Cards from '../Cards';
-
 
 class SearchForm extends Component {
-  state = {
-    allowNew: true,
-    bsSize: 'large',
-    multiple: false,
-    isLoading: false,
-    options: [],
-    searchText: ''
-  };
-
   render() {
-    const { loading, actions, projects } = this.props;
+    const { loading, actions } = this.props;
 
-    console.log(projects);
     return (
       <div>
-        <AsyncTypeahead
-          {...this.state}
-          id="SearchForm"
-          className="searchForm"
-          labelKey="full_name"
-          options={projects}
-          minLength={3}
-          onSearch={event => {
-            actions.setSearchText(event);
-            this.setState({ searchText: event });
-            actions.fetchProject();
-            this.setState({ options: projects });
-            console.log(this.state);
-          }}
-          placeholder="Search for a Github project..."
-          isLoading={loading}
-          // renderMenuItemChildren={option => (
-          //   <GithubMenuItem key={option.id} user={option} />
-          // )}
-        />
         <FormControl
           size="lg"
           placeholder="Поиск проекта на Github..."
+          minLength={3}
           onChange={event => {
             actions.setSearchText(event.target.value);
-            this.setState({ searchText: event.target.value });
             actions.fetchProject();
-            this.setState({ options: projects });
-            console.log(this.state);
           }}
         />
         <div>
-          {/* {loading ? 'Loading' : <Cards data={projects} hidden={projects[0].full_name === ''} />} */}
+          <Spinner animation="border" role="status" style={{ visibility: loading ? 'visible' : 'hidden' }} />
         </div>
       </div>
     );
@@ -66,9 +30,7 @@ class SearchForm extends Component {
 SearchForm.propTypes = {
   loading: PropTypes.bool.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
-  actions: PropTypes.object.isRequired,
-  // eslint-disable-next-line react/forbid-prop-types
-  projects: PropTypes.array.isRequired
+  actions: PropTypes.object.isRequired
 };
 
 export default SearchForm;
